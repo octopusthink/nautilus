427c2414ee8bd88caaa1e9231b0ca468ce5d9236
sarah <sarah@triggersandsparks.com>
2019-04-17 11:30:44 +0100

    Implement 8pt grid for spacing and apply to headings and paragraphs.

--- src/base/typography/typography.js
+++ src/base/typography/typography.js
@@ -9,6 +9,7 @@ import { css } from 'styled-components';
 
 import fonts from './fonts';
 import fontSizes from './fontSizes';
+import spacerSizes from './../spacing';
 
 const typography = {
   // Headings
@@ -16,26 +17,31 @@ const typography = {
     ${fonts.primaryBold};
     ${fontSizes.size6};
     line-height: 1;
+    margin-bottom: ${spacerSizes.xlarge};
   `,
   headingXLarge: css`
     ${fonts.primaryBold};
     ${fontSizes.size5};
     line-height: 1;
+    margin-bottom: ${spacerSizes.medium};
   `,
   headingLarge: css`
     ${fonts.primaryBold};
     ${fontSizes.size4};
     line-height: 1.2;
+    margin-bottom: ${spacerSizes.medium};
   `,
   headingMedium: css`
     ${fonts.primaryRegular};
     ${fontSizes.size3};
     line-height: 1.2;
+    margin-bottom: ${spacerSizes.medium};
   `,
   headingSmall: css`
     ${fonts.primaryRegular};
     ${fontSizes.size2};
     line-height: 1.4;
+    margin-bottom: ${spacerSizes.small};
   `,
   headingXSmall: css`
     ${fonts.interfaceBold};
@@ -43,6 +49,7 @@ const typography = {
     letter-spacing: 1px;
     line-height: 1.2;
     text-transform: uppercase;
+    margin-bottom: ${spacerSizes.small};
   `,
   headingXXSmall: css`
     ${fonts.interfaceBold};
@@ -50,6 +57,7 @@ const typography = {
     letter-spacing: 1px;
     line-height: 1.2;
     text-transform: uppercase;
+    margin-bottom: ${spacerSizes.small};
   `,
 
   // Body
@@ -57,26 +65,31 @@ const typography = {
     ${fonts.bodyRegular};
     ${fontSizes.size2};
     line-height: 1.6;
+    margin-bottom: ${spacerSizes.xlarge};
   `,
   bodyItalic: css`
     ${fonts.bodyItalic};
     ${fontSizes.size2};
     line-height: 1.6;
+    margin-bottom: ${spacerSizes.xlarge};
   `,
   bodyBold: css`
     ${fonts.bodyBold};
     ${fontSizes.size2};
     line-height: 1.6;
+    margin-bottom: ${spacerSizes.xlarge};
   `,
   body: css`
     ${fonts.bodyRegular};
     ${fontSizes.size1};
     line-height: 1.6;
+    margin-bottom: ${spacerSizes.xlarge};
   `,
   finePrint: css`
     ${fonts.bodyRegular};
     ${fontSizes.size0};
     line-height: 1.6;
+    margin-bottom: ${spacerSizes.xlarge};
   `,
 };
 
