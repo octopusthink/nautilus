c113d7c088cd29700046ea3371540162d0a06e0c
sarah <sarah@triggersandsparks.com>
2019-04-18 19:47:57 +0100

    Add references to form/function/foundation and rearrange documentation a wee bit to make it easier to parse.

--- docs/introduction.md
+++ docs/introduction.md
@@ -1,5 +1,37 @@
-### Hello 
-
 Nautilus is a design system, built for flexibility, efficiency, and beauty. 
 
 Nautilus is based in part on [Ether](https://ether.thescenery.co), with heavy contributions from an array of other design systems. It's a perpetual work in progress that is currently very much in progress. If you'd like to use it  for something, feel free! If you'd like to fork it and make something new, you can do that too. Nautilus is open-source, open-design, and open to contributions. Feel free to file an issue or open a PR [on Github](https://github.com/octopusthink/nautilus) if you notice anything amiss, or if you'd just like to say hi! 👋
+
+Nautilus is structured into three sections:
+
+## Foundation 
+
+Principles, guidelines, core design tokens.
+
+_Example: colour palettes_
+
+Start here if you're just getting started.
+
+## Function 
+
+Components, patterns, and layouts. Everything that's a React component. Function provides a series of components and patterns with a defined behaviour. 
+
+_Example: a button or a card_
+
+Start here if you're looking to build something right away.
+
+## Form 
+
+Form is brand expression, polish, identity, uniqueness, emotion, beauty. Form is where brand expression and beauty comes into play: essentially themeing, it's how visual styling is applied to the Functional components making use of the core rules and variables provided by the Foundation.
+
+_Example: primary colour, border-radius_
+
+Start here if you're looking to take your app/site and make it your own.
+
+## So how does it all fit together?
+
+So the colour ramps would belong in Foundation, whereas accent colours belong to Form. In css structure, this becomes the overriddable stuff, which theoretically makes for a more robustly flexible system. 
+
+So, for instance, Foundation provides a core set of colour palettes with a wide range. 
+
+This will make a whole lot more sense once the system is  a bit more fleshed out, but for now, just trust me that it'll all make  sense in time, and once there's a diagram.
--- styleguide.config.js
+++ styleguide.config.js
@@ -38,36 +38,62 @@ module.exports = {
       content: 'docs/introduction.md',
     },
     {
-      name: 'Colour Palette',
-      content: 'docs/colors.md',
-      description: 'Nautilus provides a wide range of colours to use.',
+      name: 'Foundation',
+      content: 'docs/foundation.md',
+      sections: [
+        {
+          name: 'Principles',
+          //content: 'docs/principles.md',
+          description: 'Overall principles to shape design decisions.',
+        },
+        {
+          name: 'Colour Palette',
+          content: 'docs/colors.md',
+          description: 'Nautilus provides a wide range of colours to use.',
+        },
+        {
+          name: 'Typography',
+          content: 'docs/typography.md',
+          description: 'Typography samples and variations',
+        },
+        {
+          name: 'Spacing',
+          content: 'docs/spacing.md',
+          description: 'Nautilus uses an 8pt spacing system. Get out your calculator.',
+        },
+      ],
+      sectionDepth: 2,
     },
     {
-      name: 'Colour Variables',
-      content: 'docs/colorVariables.md',
-      description: 'Variables are used as design tokens.',
+      name: 'Function',
+      content: 'docs/function.md',
+      sections: [
+        {
+          name: 'Input Components',
+          components: 'src/components/input/**/[A-Z]*.{js,jsx}',
+        },
+        {
+          name: 'Layout Components',
+          components: 'src/components/layout/**/[A-Z]*.{js,jsx}',
+        },
+        {
+          name: 'Typography Components',
+          components: 'src/components/typography/**/[A-Z]*.{js,jsx}',
+        },
+      ],
+      sectionDepth: 3,
     },
     {
-      name: 'Typography',
-      content: 'docs/typography.md',
-      description: 'Typography samples and variations',
-    },
-    {
-      name: 'Spacing',
-      content: 'docs/spacing.md',
-      description: 'Nautilus uses an 8pt spacing system. Get out your calculator.',
-    },
-    {
-      name: 'Input Components',
-      components: 'src/components/input/**/[A-Z]*.{js,jsx}',
-    },
-    {
-      name: 'Layout Components',
-      components: 'src/components/layout/**/[A-Z]*.{js,jsx}',
-    },
-    {
-      name: 'Typography Components',
-      components: 'src/components/typography/**/[A-Z]*.{js,jsx}',
+      name: 'Form',
+      content: 'docs/form.md',
+      sections: [
+        {
+          name: 'Colour Variables',
+          content: 'docs/colorVariables.md',
+          description: 'Variables are used as design tokens.',
+        },
+      ],
+      sectionDepth: 3,
     },
   ],
 };
