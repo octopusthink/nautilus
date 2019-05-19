a99fbe30dbeb40013dd0f732bfbf2d82d333e7e5
sarah <sarah@triggersandsparks.com>
2019-05-19 23:12:50 +0100

    Add global styles for better type rendering in browsers.

--- src/styleguide/ThemeWrapper/index.js
+++ src/styleguide/ThemeWrapper/index.js
@@ -1,3 +1,4 @@
+import { Global, css } from '@emotion/core';
 import { ThemeProvider } from 'emotion-theming';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -5,7 +6,26 @@ import React from 'react';
 import { nautilus } from 'themes';
 
 export const ThemeWrapper = ({ children }) => {
-  return <ThemeProvider theme={nautilus}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={nautilus}>
+      <Global
+        styles={css`
+          html {
+            /* Make type rendering look crisper */
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+
+            /* Disable auto-enlargement of small text in Safari */
+            text-size-adjust: 100%;
+
+            /* Enable kerning and optional ligatures */
+            text-rendering: optimizeLegibility;
+          }
+        `}
+      />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 ThemeWrapper.defaultProps = {
