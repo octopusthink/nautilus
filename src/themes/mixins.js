cc37f134a529d1a6eca54545673fd0dc44be6744
sarah <sarah@triggersandsparks.com>
2019-05-20 17:14:21 +0100

    Add styling for headings.

--- src/themes/mixins.js
+++ src/themes/mixins.js
@@ -14,7 +14,7 @@ const calculateLineHeight = ({ fontSize, targetLineHeight }) => {
   // Now we check to see if the font size * line-height is divisible by 4
   // If not, increment until it is divisible by four.
   // This ensures that line-height respects our vertical rhythm.
-  let targetLineHeightInPixels = Math.round(targetLineHeight * fontSize);
+  let targetLineHeightInPixels = Math.ceil(targetLineHeight * fontSize);
 
   // Increment the target line height until it's a multiple of 4
   while (targetLineHeightInPixels % 4 !== 0) {
@@ -27,47 +27,148 @@ const calculateLineHeight = ({ fontSize, targetLineHeight }) => {
   return lineHeight;
 };
 
-const outputTypeDetails = ({ theme, sizeNumber }) => {
+// Output typographic styling to apply to elements.
+const outputTypeDetails = ({ theme, sizeNumber, font }) => {
   const fontSize = calculateFontSize({
     starterSize: theme.typography.starterSizes.desktop,
     scaleModifier: theme.typography.scaleModifiers.desktop,
     sizeNumber,
   });
 
+  // Get the target line height from our theme for the font used.
+  let targetLineHeight;
+  if (font === 'headings') {
+    targetLineHeight = theme.typography.lineHeights.headings;
+  } else if (font === 'interface') {
+    targetLineHeight = theme.typography.lineHeights.interface;
+  } else {
+    targetLineHeight = theme.typography.lineHeights.body;
+  }
+
   return {
     fontSize: `${fontSize}rem`,
     lineHeight: calculateLineHeight({
       fontSize: fontSize * 10,
-      targetLineHeight: theme.typography.lineHeights.interface,
+      targetLineHeight,
     }),
   };
 };
 
-// Define our font styles here, then pull them into the theme.
+// Interface
 export const interfaceSmall = (theme) => {
   return {
     fontFamily: theme.typography.fonts.interface,
-    ...outputTypeDetails({ theme, sizeNumber: -0.5 }),
+    ...outputTypeDetails({ theme, sizeNumber: -0.5, font: 'interface' }),
     fontWeight: 500,
-    fontStyle: 'normal',
   };
 };
 
-// Define our font styles here, then pull them into the theme.
 export const interfaceMedium = (theme) => {
   return {
     fontFamily: theme.typography.fonts.interface,
-    ...outputTypeDetails({ theme, sizeNumber: 0 }),
+    ...outputTypeDetails({ theme, sizeNumber: 0, font: 'interface' }),
     fontWeight: 500,
-    fontStyle: 'normal',
   };
 };
 
 export const interfaceLarge = (theme) => {
   return {
     fontFamily: theme.typography.fonts.interface,
-    ...outputTypeDetails({ theme, sizeNumber: 1 }),
+    ...outputTypeDetails({ theme, sizeNumber: 1, font: 'interface' }),
     fontWeight: 500,
-    fontStyle: 'normal',
   };
 };
+
+// Body
+export const bodySmall = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.body,
+    ...outputTypeDetails({ theme, sizeNumber: 0.5, font: 'body' }),
+    fontWeight: 400,
+  };
+};
+
+export const bodyMedium = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.body,
+    ...outputTypeDetails({ theme, sizeNumber: 0, font: 'body' }),
+    fontWeight: 400,
+  };
+};
+
+export const bodyLarge = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.body,
+    ...outputTypeDetails({ theme, sizeNumber: 1, font: 'body' }),
+    fontWeight: 400,
+  };
+};
+
+// Headings
+export const headingSmall = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.headings,
+    ...outputTypeDetails({ theme, sizeNumber: 3, font: 'headings' }),
+    fontWeight: 600,
+  };
+};
+
+export const headingMedium = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.headings,
+    ...outputTypeDetails({ theme, sizeNumber: 4, font: 'headings' }),
+    fontWeight: 600,
+  };
+};
+
+export const headingLarge = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.headings,
+    ...outputTypeDetails({ theme, sizeNumber: 5, font: 'headings' }),
+    fontWeight: 600,
+  };
+};
+
+export const pageTitle = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.headings,
+    ...outputTypeDetails({ theme, sizeNumber: 6, font: 'headings' }),
+    fontWeight: 600,
+  };
+};
+
+export const subtitle = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.headings,
+    ...outputTypeDetails({ theme, sizeNumber: 2, font: 'headings' }),
+    fontWeight: 400,
+    fontStyle: 'italic',
+  };
+};
+
+// Metadata
+export const metadataSmall = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.interface,
+    ...outputTypeDetails({ theme, sizeNumber: -0.5, font: 'interface' }),
+    fontWeight: 500,
+    textTransform: 'small-caps',
+  };
+};
+
+export const metadataLarge = (theme) => {
+  return {
+    fontFamily: theme.typography.fonts.interface,
+    ...outputTypeDetails({ theme, sizeNumber: 1, font: 'interface' }),
+    fontWeight: 500,
+    textTransform: 'small-caps',
+  };
+};
+
+// Other font styles we should generate:
+// Interface L M S
+// Body L M S
+// Heading L M S
+// Page Title
+// Subtitle
+// Metadata L S
