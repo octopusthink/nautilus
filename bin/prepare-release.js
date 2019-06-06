38f9914fec2fff1296db8badf7d0aab8259c6c2f
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-06-06 03:32:41 +0100

    fix: Final attempt at release

--- bin/prepare-release.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const shell = require('shelljs');
-
-const { NPM_TOKEN } = process.env;
-
-shell.exec(`echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} > .npmrc`, {
-  silent: true,
-});
