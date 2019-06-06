f8587812b7d1b5de74ce0ca20d2eccb11600029a
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-06-06 03:25:31 +0100

    fix: Tweak build process further...

--- bin/prepare-release.js
+++ bin/prepare-release.js
@@ -3,6 +3,6 @@ const shell = require('shelljs');
 
 const { NPM_TOKEN } = process.env;
 
-shell.exec(`echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} > ~/.npmrc`, {
+shell.exec(`echo //registry.npmjs.org/:_authToken=${NPM_TOKEN} > .npmrc`, {
   silent: true,
 });
