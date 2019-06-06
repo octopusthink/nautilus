d96a38e2447525cbb136fb213fd5e1ecbacd0cc2
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-06-06 03:03:21 +0100

    fix: Use weird NPM string for release

--- bin/prepare-release.js
+++ bin/prepare-release.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const shell = require('shelljs');
 
-const { NPM_TOKEN } = process.env;
-
-const authInfoString = `//registry.npmjs.org/:_authToken=${NPM_TOKEN}`;
+// eslint-disable-next-line no-template-curly-in-string
+const authInfoString = '//registry.npmjs.org/:_authToken=${NPM_TOKEN}';
 
 shell.exec(`${authInfoString} > ~/.npmrc`, { silent: true });
