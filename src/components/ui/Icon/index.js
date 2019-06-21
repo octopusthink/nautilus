42b7e5a6f25661d702a3f22e31a5a5516440d2bb
sarah <sarah@triggersandsparks.com>
2019-06-21 23:17:15 +0200

    Attempt to add size props to Icon component.

--- src/components/ui/Icon/index.js
+++ src/components/ui/Icon/index.js
@@ -5,23 +5,43 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import SVG from 'react-inlinesvg';
 
+const LARGE = 48;
+const MEDIUM = 24;
+const SMALL = 16;
+
+const IconSizes = [LARGE, MEDIUM, SMALL];
+
 export const Icon = (props) => {
-  const { children, name, ...otherProps } = props;
+  const { children, name, size, color, ...otherProps } = props;
 
   return (
-    <SVG src={`icons/${name}.svg`} wrapper={React.createFactory('span')}>
+    <SVG
+      css={css`
+        height: 128px;
+        width: 128px;
+      `}
+      src={`icons/${name}.svg`}
+      wrapper={React.createFactory('span')}
+    >
       <img src={`icons/${name}.svg`} />
     </SVG>
   );
 };
 
 export const styles = ({ theme, ...otherProps }) => {
-  return css``;
+  return css`
+    svg {
+      height: 128px;
+      width: 128px;
+    }
+  `;
 };
 
 Icon.defaultProps = {
   children: undefined,
   name: undefined,
+  size: 'medium',
+  color: undefined,
 };
 
 Icon.propTypes = {
@@ -29,6 +49,10 @@ Icon.propTypes = {
   children: PropTypes.node,
   /** The name of the icon to use. */
   name: PropTypes.string,
+  /** Change the size of the icon. */
+  size: PropTypes.oneOf(IconSizes),
+  /** Apply colour to the icon. */
+  color: PropTypes.string,
 };
 
 export const { defaultProps, propTypes } = Icon;
