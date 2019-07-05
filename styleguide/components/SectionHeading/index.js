3bbb95490dd67cef79d5adf898693cc0a92c1a1d
sarah <sarah@triggersandsparks.com>
2019-07-06 00:22:49 +0100

    style: Indentation fixes for Captain Pedantic.

--- styleguide/components/SectionHeading/index.js
+++ styleguide/components/SectionHeading/index.js
@@ -5,30 +5,40 @@ import Heading from 'rsg-components/Heading';
 
 import theme from 'styleguide/theme';
 
-function SectionHeadingRenderer({ children, toolbar, id, href, depth, deprecated }) {
-	// const headingLevel = Math.min(6, depth);
-	// Overwrite RSG's classification of headings, since we're only using top-level headings here!
-	const headingLevel = 1;
+function SectionHeadingRenderer({
+  children,
+  toolbar,
+  id,
+  href,
+  depth,
+  deprecated,
+}) {
+  // const headingLevel = Math.min(6, depth);
+  // Overwrite RSG's classification of headings, since we're only using top-level headings here!
+  const headingLevel = 1;
 
-	return (
-			<Heading level={headingLevel} id={id}>
-				<a href={href} css={css`
-					text-decoration: none;
-					color: ${theme.colors.text.default};
-				`}>
-					{children}
-				</a>
-			</Heading>
-	);
+  return (
+    <Heading level={headingLevel} id={id}>
+      <a
+        href={href}
+        css={css`
+          text-decoration: none;
+          color: ${theme.colors.text.default};
+        `}
+      >
+        {children}
+      </a>
+    </Heading>
+  );
 }
 
 SectionHeadingRenderer.propTypes = {
-	children: PropTypes.node,
-	toolbar: PropTypes.node,
-	id: PropTypes.string.isRequired,
-	href: PropTypes.string.isRequired,
-	depth: PropTypes.number.isRequired,
-	deprecated: PropTypes.bool,
+  children: PropTypes.node,
+  toolbar: PropTypes.node,
+  id: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  depth: PropTypes.number.isRequired,
+  deprecated: PropTypes.bool,
 };
 
 export default SectionHeadingRenderer;
