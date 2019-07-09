95e714464a7c4e3fd279b7510e63e1761f8d9f01
sarah <sarah@triggersandsparks.com>
2019-07-09 13:01:52 +0100

    fix: Tweak margins and spacing for different breakpoints.

--- styleguide/components/StyleGuide/index.js
+++ styleguide/components/StyleGuide/index.js
@@ -86,6 +86,10 @@ export const StyleGuide = ({
           h3 {
             margin-top: ${toUnits(theme.spacing.margin.large)} !important;
           }
+
+          img {
+            max-width: 100%;
+          }
         `}
       />
 
@@ -101,28 +105,32 @@ export const StyleGuide = ({
               top: 0;
               z-index: 1000;
 
-              ${showSidebar && css`
-                overflow: auto;
-              `}
-
               @media (max-width: 767px) {
                 height: 76px;
                 left: 0;
                 right: 0;
-
-                ${showSidebar && css`
-                  height: 100%;
-                `}
+                padding: 0 ${toUnits(theme.spacing.padding.large)};
               }
 
               @media (min-width: 768px) {
                 left: -240px;
                 width: 300px;
-
-                ${showSidebar && css`
-                  left: 0;
-                `}
+                padding: 0 ${toUnits(theme.spacing.padding.extraLarge)};
               }
+
+              ${showSidebar && css`
+                overflow: auto;
+
+                @media (max-width: 767px) {
+                  height: 100%;
+                }
+
+                @media (min-width: 768px) {
+                  left: 0;
+                }
+              `}
+
+
             `}
             onClick={() => {
               if (isMobile) {
@@ -137,35 +145,38 @@ export const StyleGuide = ({
             }}
             ref={menuRef}
           >
-          <a href="#" css={css`
-            ${metadata.large(theme)};
-            display: flex;
-            text-decoration: none;
-            color: ${theme.colors.neutral.grey200};
-            width: 100%;
-            align-items: center;
-            justify-content: space-between;
-          `}
-          onClick = {toggleSidebar}
-          >
-            Nautilus
-            <div css={css`
-              ${metadata.small(theme)};
-              display: flex;
-              flex-direction: column;
-              font-size: 1.2rem;
-              text-decoration: none;
-              height: 76px;
-              width: 60px;
-              align-items: center;
-              justify-content: center;
-              margin-right: -${toUnits(theme.spacing.padding.large)};
-            `} >
-            <Icon name="menu" background="white" border="#cd2f83" background="hotpink" />
-          </div>
-        </a>
+            <a
+              href="#"
+              css={css`
+                ${metadata.large(theme)};
+                display: flex;
+                text-decoration: none;
+                color: ${theme.colors.neutral.grey200};
+                width: 100%;
+                align-items: center;
+                justify-content: space-between;
+              `}
+              onClick = {toggleSidebar}
+            >
+              Nautilus
+              <div
+                css={css`
+                  ${metadata.small(theme)};
+                  display: flex;
+                  flex-direction: column;
+                  font-size: 1.2rem;
+                  text-decoration: none;
+                  height: 76px;
+                  width: 60px;
+                  align-items: center;
+                  justify-content: center;
+                  margin-right: -${toUnits(theme.spacing.padding.extraLarge)};
+                `}
+              >
+                <Icon name="menu" background="white" border="#cd2f83" background="hotpink" />
+              </div>
+            </a>
             {toc}
-
           </div>
         )}
 
@@ -178,18 +189,20 @@ export const StyleGuide = ({
 
           @media (min-width: 768px) {
             margin-left: 60px;
-            padding: ${toUnits(theme.spacing.margin.medium)} ${toUnits(theme.spacing.margin.xxl)};
+            padding: ${toUnits(theme.spacing.margin.medium)};
             max-width: 800px;
+          }
 
-            ${showSidebar && css`
+          @media screen and (min-width: 980px) {
+            padding-left: ${toUnits(theme.spacing.margin.xxl)};
+            padding-right: ${toUnits(theme.spacing.margin.xxl)};
+          }
+
+          ${showSidebar && css`
+            @media (min-width: 768px) {
               margin-left: 300px;
-            `}
-          }
-
-          img {
-            max-width: 100%;
-          }
-
+            }
+          `}
         `}>{children}
 
         <Footer>
