da1bf9342596eff965f916af2d4c8a247f0090ec
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2019-08-29 20:09:48 -0400

    fix: Lint error with unused variables

--- src/components/ui/SkipLink/index.js
+++ src/components/ui/SkipLink/index.js
@@ -2,10 +2,8 @@ import { css } from '@emotion/core';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import Link, {
-  ComponentClassName as LinkComponentClassName,
-} from 'components/ui/Link';
-import VisuallyHidden, {
+import Link from 'components/ui/Link';
+import {
   VisuallyHiddenStyles,
   VisuallyHiddenRevealStyles,
 } from 'components/ui/VisuallyHidden';
