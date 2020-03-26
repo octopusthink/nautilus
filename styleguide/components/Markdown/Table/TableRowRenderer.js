cec41cc1f1de195ee80f03081ba1374f5725befd
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-03-26 00:02:38 +0000

    chore: Use prettier on more files

--- styleguide/components/Markdown/Table/TableRowRenderer.js
+++ styleguide/components/Markdown/Table/TableRowRenderer.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export function TableRowRenderer({ children }) {
-	return <tr>{children}</tr>;
+  return <tr>{children}</tr>;
 }
 TableRowRenderer.propTypes = {
-	children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default TableRowRenderer;
--- styleguide/components/slots/UsageTabButton.js
+++ styleguide/components/slots/UsageTabButton.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import TabButton from 'rsg-components/TabButton';
 import isEmpty from 'lodash/isEmpty';
 
-const UsageTabButton = props => {
-	const component = props.props;
-	const showButton = !isEmpty(component.props) || !isEmpty(component.methods);
-	return showButton ? <TabButton {...props}>Props</TabButton> : null;
+const UsageTabButton = (props) => {
+  const component = props.props;
+  const showButton = !isEmpty(component.props) || !isEmpty(component.methods);
+  return showButton ? <TabButton {...props}>Props</TabButton> : null;
 };
 
 UsageTabButton.propTypes = {
-	onClick: PropTypes.func.isRequired,
-	name: PropTypes.string.isRequired,
-	props: PropTypes.shape({
-		props: PropTypes.array,
-		methods: PropTypes.array,
-	}).isRequired,
-	active: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  props: PropTypes.shape({
+    props: PropTypes.array,
+    methods: PropTypes.array,
+  }).isRequired,
+  active: PropTypes.bool,
 };
 
 export default UsageTabButton;
