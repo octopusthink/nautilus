5aee8ea9b54d172c94f142dae0813571bec219a4
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2020-05-08 16:37:37 +0100

    chore: Add styleguide helper components

---
+++ styleguide/nautilus-styleguide-components/Flex/index.js
@@ -0,0 +1,17 @@
+import { css } from '@emotion/core';
+import React from 'react';
+
+const Flex = ({ children }) => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-wrap: 'wrap';
+      `}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Flex;
