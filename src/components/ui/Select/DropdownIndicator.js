ba6981d0f9b95dfd32fa4ed06ba71947a41e9802
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2021-06-26 20:39:48 +0100

    feat: Hide plus button when focused.

--- src/components/ui/Select/DropdownIndicator.js
+++ src/components/ui/Select/DropdownIndicator.js
@@ -3,10 +3,10 @@ import React from 'react';
 import Icon from '../Icon';
 
 const DropdownIndicator = (props) => {
-  const { selectProps } = props;
+  const { isFocused, selectProps } = props;
 
   if (selectProps.isMulti) {
-    return <Icon name="plus" />;
+    return isFocused ? null : <Icon name="plus" />;
   }
 
   return <Icon name={selectProps.menuIsOpen ? 'chevron-up' : 'chevron-down'} />;
@@ -17,7 +17,9 @@ DropdownIndicator.defaultProps = {
 };
 
 DropdownIndicator.propTypes = {
-  /* @ignore Don't output any CSS styles. */
+  /** @ignore */
+  isFocused: PropTypes.bool.isRequired,
+  /** @ignore Don't output any CSS styles. */
   // eslint-disable-next-line react/forbid-prop-types
   selectProps: PropTypes.object,
 };
